e1f788558e4fe37abb54391cf640697cd16755ac
DucViet0301 <ducviet0301@gmail.com>
2026-05-13 18:15:47 +0700

    Chức năng đăng nhập

--- LoginApp/Form1.cs
+++ LoginApp/Form1.cs
@@ -16,5 +16,37 @@ namespace LoginApp
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string userEmail = txtUsername.Text.Trim();
+            string userPass = txtPassword.Text.Trim();
+
+           
+            string correctEmail = "0030168@huce.edu.vn";
+            string correctMSSV = "0030168";
+
+            
+            if (userEmail == correctEmail && userPass == correctMSSV)
+            {
+                
+                MessageBox.Show("Đăng nhập thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+               
+                MessageBox.Show("Đăng nhập thất bại", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
